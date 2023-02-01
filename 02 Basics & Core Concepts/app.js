4a616d91a83e9c8e5ae4b03ec2557c2ddaf5643d
Mia Evans <54636763+miamija7@users.noreply.github.com>
2023-02-01 15:45:22 -0600

    raw HTML

--- 02 Basics & Core Concepts/app.js
+++ 02 Basics & Core Concepts/app.js
@@ -1,9 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn Vue!',
+            courseGoal: 'Learn Vue!',
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: 'Master Vue and build amazing apps!',
+            courseGoalHtml: '<h3>Dance!</h3>',
             vueLink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randNum = Math.random();
+            if (randNum < 0.5){
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     }
 });
 
