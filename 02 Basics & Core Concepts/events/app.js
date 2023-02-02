e4eaaf98852fe1a4c4fa7dbf0cd8563ec893c013
Mia Evans <54636763+miamija7@users.noreply.github.com>
2023-02-02 15:28:05 -0600

    event binding

--- 02 Basics & Core Concepts/events/app.js
+++ 02 Basics & Core Concepts/events/app.js
@@ -2,8 +2,35 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
+      email: ''
     };
-  },
+  }, 
+  methods: {
+    add(num) {
+      if (this.counter < 10) {this.counter = this.counter + num}
+    },
+    minus(num) {
+      if(this.counter > 0) {this.counter = this.counter - num}
+    },
+    setName(e){
+      this.name = e.target.value;
+    },
+    submitForm(e){
+      alert('Submitted!');
+      // e.target[0].value = '';
+      this.email = '';
+
+    }, 
+    confirmInput(){
+      this.confirmedName = this.name;
+    },
+    resetInput() {
+      this.name = '';
+    }
+
+  }
 });
 
 app.mount('#events');
